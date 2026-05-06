7fa2bf841bbad66c0d560ecd5fb93a70572f8083
meapanupong <meapanupong@central.co.th>
2026-05-06 13:38:11 +0700

    fix ga4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideGoogleAnalytics, provideGoogleAnalyticsRouter } from 'ngx-google-analytics';
+import { provideGoogleAnalytics, provideGoogleAnalyticsRouter } from '@hakimio/ngx-google-analytics';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
